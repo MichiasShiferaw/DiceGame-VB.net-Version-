3b213e0f3a418f3049f0fcad9d9b78741a050e09
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-15 09:49:35 -0500

    Declare All The Variables

--- DiceGame.vb
+++ DiceGame.vb
@@ -2,3 +2,9 @@
 Imports System
 
 Public Class DiceGame
+    Private Sub DiceGame_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+       'Declare nescarry variable 
+        Dim roll As New Random()
+        Dim randomNumber As Integer = roll.Next(6)
+        Dim guess As Integer
+        Dim counter As Integer
